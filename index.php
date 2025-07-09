97e81b1bf870f36e678606d0612793ba19ca24c5
Alora Mia <xaloramia@gmail.com>
2025-07-09 19:49:40 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+// Set the Content-Type header to ensure proper display of response
+header('Content-Type: text/plain');
+
+// Output a simple message to confirm that the server is up and running
+echo "Server is up and running! Uptime Robot is keeping me alive.";
+?>
