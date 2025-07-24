95e678d3ec5214372cdc8ca40ffa270c6a205d2c
Alora Mia <xaloramia@gmail.com>
2025-07-24 21:30:31 +0900

    Update generate.php

--- license/generate.php
+++ license/generate.php
@@ -6,11 +6,11 @@ $key = $_POST['key'] ?? 'unknown';
 $resource = $_POST['resource'] ?? 'unknown';
 $fake_code = substr(md5($key . $resource), 0, 8);
 
-// Log attempt
-$log = date('c') . " | GENERATE | IP: {$_SERVER['REMOTE_ADDR']} | key: $key | resource: $resource\n";
-file_put_contents(__DIR__ . '/../../piracy.log', $log, FILE_APPEND);
+// Output to web server log / console
+error_log("DREAMAPI > GENERATE | IP: {$_SERVER['REMOTE_ADDR']} | key=$key | resource=$resource");
 
 echo json_encode([
-  'code' => $fake_code, // looks valid, always wrong
+  'code' => $fake_code,
   'key' => $key
 ]);
+exit;
