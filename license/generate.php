b79654d98fa9f756cd74cbc40f3f68ff1f034685
Alora Mia <xaloramia@gmail.com>
2025-07-24 21:42:07 +0900

    Update generate.php

--- license/generate.php
+++ license/generate.php
@@ -1,16 +1,9 @@
 <?php
+http_response_code(403); // Forbidden
 header('Content-Type: application/json');
-http_response_code(200);
-
-$key = $_POST['key'] ?? 'unknown';
-$resource = $_POST['resource'] ?? 'unknown';
-$fake_code = substr(md5($key . $resource), 0, 8);
-
-// Output to web server log / console
-error_log("DREAMAPI > GENERATE | IP: {$_SERVER['REMOTE_ADDR']} | key=$key | resource=$resource");
 
 echo json_encode([
-  'code' => $fake_code,
-  'key' => $key
+  'error' => 'Access denied',
+  'status' => 403
 ]);
 exit;
