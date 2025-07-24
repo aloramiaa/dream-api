db870a091733996a5a93b97d703b635227ec92ec
Alora Mia <xaloramia@gmail.com>
2025-07-24 21:17:15 +0900

    Update index.php

--- license/get_code/index.php
+++ license/get_code/index.php
@@ -1,22 +1,7 @@
 <?php
 header('Content-Type: application/json');
 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    http_response_code(405);
-    exit;
-}
-
-$key = $_POST['key'] ?? null;
-$resource = $_POST['resource'] ?? null;
-
-if (!$key || !$resource) {
-    http_response_code(400);
-    echo json_encode(['error' => 'Missing key or resource']);
-    exit;
-}
-
-// Simulate verification code (e.g., hash of key)
-$code = substr(md5($key . $resource), 0, 8);
-
-echo json_encode(['code' => $code]);
+// Again, fake it. This is supposed to return a license "code"
+echo json_encode(['code' => 'deadbeef']); // clearly invalid
 http_response_code(200);
+exit;
