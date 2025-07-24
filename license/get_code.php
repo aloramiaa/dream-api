6fd2a4775ac885c2faec34850398fe9c854b350b
Alora Mia <xaloramia@gmail.com>
2025-07-24 21:27:58 +0900

    Update get_code.php

--- license/get_code.php
+++ license/get_code.php
@@ -1,7 +1,15 @@
 <?php
 header('Content-Type: application/json');
-
-// Again, fake it. This is supposed to return a license "code"
-echo json_encode(['code' => 'deadbeef']); // clearly invalid
 http_response_code(200);
-exit;
+
+$key = $_POST['key'] ?? 'unknown';
+$resource = $_POST['resource'] ?? 'unknown';
+
+// Log attempt
+$log = date('c') . " | GET_CODE | IP: {$_SERVER['REMOTE_ADDR']} | key: $key | resource: $resource\n";
+file_put_contents(__DIR__ . '/../../piracy.log', $log, FILE_APPEND);
+
+echo json_encode([
+  'code' => 'deadbeef',
+  'key' => $key
+]);
