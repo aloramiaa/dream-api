880617f37e69aa877a66c813b283a23d82e5d6cd
Alora Mia <xaloramia@gmail.com>
2025-07-24 21:30:40 +0900

    Update get_code.php

--- license/get_code.php
+++ license/get_code.php
@@ -5,11 +5,11 @@ http_response_code(200);
 $key = $_POST['key'] ?? 'unknown';
 $resource = $_POST['resource'] ?? 'unknown';
 
-// Log attempt
-$log = date('c') . " | GET_CODE | IP: {$_SERVER['REMOTE_ADDR']} | key: $key | resource: $resource\n";
-file_put_contents(__DIR__ . '/../../piracy.log', $log, FILE_APPEND);
+// Output to console
+error_log("DREAMAPI > GET_CODE | IP: {$_SERVER['REMOTE_ADDR']} | key=$key | resource=$resource");
 
 echo json_encode([
   'code' => 'deadbeef',
   'key' => $key
 ]);
+exit;
